df1bdcd38b73c93d5fa76a9cf4cd55ed0c7c164e
Delery5 <harrisondelery@yahoo.com>
2024-07-10 09:34:59 -0500

    Edited controller class

--- src/main/java/com/delerycarmate/controllers/HomeController.java
+++ src/main/java/com/delerycarmate/controllers/HomeController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-//controller class
+
+    //Homecontroller class
     @GetMapping("/")
     public String showHomePage() {
         return "index";
